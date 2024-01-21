8a690621f25eb9483ba73c6fb585ebf55c68c525
Lauro Langosco di Langosco <langosco.lauro@gmail.com>
2024-01-21 17:11:53 +0000

    improve mem usage in hacky script to validate compiling

--- experiments/hacky_validate_compiling.py
+++ experiments/hacky_validate_compiling.py
@@ -3,10 +3,8 @@ from tracr.rasp import rasp
 from tracr.compiler import compiling
 from rasp_generator import sampling, utils
 import numpy as np
-import tracemalloc
+import jax
 
-
-tracemalloc.start()
 
 rng = np.random.default_rng()
 
@@ -19,28 +17,57 @@ def compile_rasp_to_model(sop: rasp.SOp, vocab={0,1,2,3,4}, max_seq_len=5, compi
     )
 
 
-def sample_test_input(rng, vocab={0,1,2,3,4}, max_seq_len=5):
-    seq_len = rng.choice(range(1, max_seq_len+1))
-    return rng.choice(list(vocab), size=seq_len).tolist()
-
-
-test_inputs = [sample_test_input(rng) for _ in range(100)]
+test_inputs = [utils.sample_test_input(rng) for _ in range(100)]
 test_inputs += [[0], [0,0,0,0,0], [4,4,4,4], [0,1,2,3]]
-
-
-
-n_samples = 500
+n_samples = 200
 errs = defaultdict(list)
 results = []
+
+
+def sample_program():
+    sampler = sampling.ProgramSampler(rng=rng)
+    retries = sampler.sample(n_sops=30)
+    return sampler.program, retries
+
+
+
+def validate_compiled(program: rasp.SOp, model):
+    for test_input in test_inputs:
+        rasp_out = r['program'](test_input)
+        rasp_out_sanitized = [0 if x is None else x for x in rasp_out]
+        assert isinstance(test_input, list)
+        model_out = model.apply(["BOS"] + test_input).decoded[1:]
+        if not np.allclose(model_out, rasp_out_sanitized, rtol=1e-3, atol=1e-3):
+            raise ValueError(f"Compiled program {r['program'].label} does not match RASP output.")
+
+
+def print_info_and_clear_caches():
+    print("Step", i)
+    total_compiled = sum([r['compiled'] for r in results])
+    print("Total programs compiled:", total_compiled)
+    print("Total compilation errors:", sum([not r['compiled'] for r in results[:i]]))
+    print()
+    print("Total programs compiled validly (relative to test inputs):", 
+            sum([r['validated'] for r in results]))
+    compiled = [r for r in results if r['compiled']]
+    print("Validation errors:", sum([not r['validated'] for r in compiled]))
+    print()
+
+    jax.clear_caches()
+    jax.clear_backends()
+
 
 
 print(f"Sampling {n_samples} programs...")
 for _ in range(n_samples):
     try:
-        sampler = sampling.ProgramSampler(rng=rng)
-        retries = sampler.sample(n_sops=30)
+        program, retries = sample_program()
         errs['retries'] += retries
-        results.append(dict(program=sampler.program))
+        results.append(dict(
+            program=program,
+            compiled=False,
+            validated=False,
+        ))
     except Exception as err:
         errs['sampling'].append(err)
     
@@ -54,60 +81,18 @@ print()
 
 
 for i, r in enumerate(results):
-    if 'program' not in r:
-        continue
-
-    # compile
     try:
         model = compile_rasp_to_model(r['program'])
         r['compiled'] = True
+
+        validate_compiled(r['program'], model)
+        r['validated'] = True
     except Exception as err:
-        errs['compilation'].append(err)
-        r['compilation_error'] = err
-        continue
-
-    # validate
-    try:
-        for test_input in test_inputs:
-            rasp_out = r['program'](test_input)
-            rasp_out_sanitized = [0 if x is None else x for x in rasp_out]
-            assert isinstance(test_input, list)
-            model_out = model.apply(["BOS"] + test_input).decoded[1:]
-            if not np.allclose(model_out, rasp_out_sanitized, rtol=1e-3, atol=1e-3):
-                raise ValueError(f"Compiled program {r['program'].label} does not match RASP output.\n"
-                                    f"Compiled output: {model_out}\n"
-                                    f"RASP output: {rasp_out}\n"
-                                    f"Test input: {test_input}.")
-    except Exception as err:
-            errs['validation'].append(err)
-            r['validation_error'] = err
-    
-
-    if i % 10 == 0:
-        print()
-        print("Step", i)
-        mem_snap = tracemalloc.take_snapshot()
-        stats = mem_snap.statistics('lineno')
-        for stat in stats[:5]:
-            print(stat)
-
-        print()
-        utils.display_top(mem_snap)
-        print()
-    
+        pass
 
 
+    if i % 50 == 0:
+        print_info_and_clear_caches()
 
-
-total_compiled = len([r for r in results if 'compiled' in r])
 print("Done compiling.")
-print("Total programs compiled:", total_compiled)
-print("Total compilation errors:", len(errs['compilation']))
-print("Now validating...")
-print()
-
-
-print("Total programs compiled validly (relative to test inputs):", 
-      total_compiled - len(errs['validation']))
-print("Validation errors:", len(errs['validation']))
-print()
+print_info_and_clear_caches()
