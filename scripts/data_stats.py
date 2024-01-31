f5a82479ae85778cf2fba4b34117fedf26740ac4
Lauro Langosco di Langosco <langosco.lauro@gmail.com>
2024-01-31 15:20:26 +0000

    add script to compute data statistics

---
+++ scripts/data_stats.py
@@ -0,0 +1,53 @@
+import os
+os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
+import numpy as np
+import matplotlib.pyplot as plt
+from rasp_tokenizer.data_utils import load_deduped
+
+
+# Data loading
+data = load_deduped(name='train', flatten=False)
+print("Total datapoints:", len(data))
+
+
+# Preprocessing data for histograms
+n_sops = [x['n_sops'] for x in data]
+weights_per_model = [
+    sum(len(layer['weights']) for layer in d['weights_and_tokens']
+    ) for d in data
+]
+layer_lengths = [len(x['weights_and_tokens']) for x in data]
+data_per_layer = [l for x in data for l in x['weights_and_tokens']]
+token_lens = [len(x['rasp_tok']) for x in data_per_layer]
+weight_lens = [len(x['weights']) for x in data_per_layer]
+
+
+# Plotting
+fig, axs = plt.subplots(3, 2, figsize=(12, 15))
+axs = axs.flatten()
+
+axs[0].set_title("Distribution of Program Lengths")
+axs[0].hist(n_sops, bins=range(0, 15))
+axs[0].set_xticks(range(0, 15))
+
+axs[1].set_title("Distribution of Weights per Model")
+axs[1].hist(weights_per_model)
+
+axs[2].set_title("Layer Lengths")
+axs[2].hist(layer_lengths)
+
+axs[3].set_title("Distribution of Tokens per Layer")
+axs[3].hist(token_lens, bins=range(2, 20))
+
+axs[4].set_title("Distribution of Weight Lengths per Layer")
+axs[4].hist(weight_lens)
+
+plt.tight_layout()
+plt.show()
+
+
+# Statistics
+print("Min weights per model:", np.min(weights_per_model))
+print("Max weights per model:", np.max(weights_per_model))
+print("Min weights per layer", np.min(weight_lens))
+print("Max weights per layer", np.max(weight_lens))
