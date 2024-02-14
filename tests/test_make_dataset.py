8308c4193ba374e65d74c1e8e8bbbbb10670a82d
Lauro Langosco di Langosco <langosco.lauro@gmail.com>
2024-02-13 20:08:03 -0800

    add tests for tokenization

---
+++ tests/test_make_dataset.py
@@ -0,0 +1,78 @@
+import os
+import pytest
+import numpy as np
+import shutil
+
+
+from decompile_tracr.tokenizing import tokenizer
+from decompile_tracr.dataset import config
+from decompile_tracr.dataset import generate
+from decompile_tracr.dataset import tokenize_lib
+from decompile_tracr.dataset import dedupe
+from decompile_tracr.dataset import compile
+from decompile_tracr.dataset import utils
+
+
+BASE_DIR = config.data_dir / "test_make_dataset"
+rng = np.random.default_rng(42)
+
+@pytest.fixture(scope="module")
+def make_test_data():
+    shutil.rmtree(BASE_DIR, ignore_errors=True)
+    os.makedirs(BASE_DIR)
+
+    sampler_kwargs = {
+        "n_sops": 10,
+        "min_length": 4,
+        "max_length": None
+    }
+
+    generate.generate(
+        rng, 
+        ndata=5, 
+        name='testing_make_dataset', 
+        savedir=BASE_DIR / "unprocessed",
+        **sampler_kwargs,
+    )
+
+    tokenize_lib.tokenize_lib(savedir=BASE_DIR / "unprocessed")
+
+    dedupe.dedupe(
+        loaddir=BASE_DIR / "unprocessed",
+        savedir=BASE_DIR / "deduped",
+    )
+
+    compile.compile(
+        loaddir=BASE_DIR / "deduped",
+        savedir=BASE_DIR / "full",
+    )
+    return None
+
+
+def test_sampling(make_test_data):
+    pass
+
+
+
+def test_deduplication(make_test_data):
+    pass
+
+
+def test_tokenization(make_test_data):
+    """De-tokenize, then re-tokenize. Ensure that the re-tokenized
+    program is the same as the original."""
+    deduped_dirs = list((BASE_DIR / "deduped").iterdir())
+    deduped_dirs = [x for x in deduped_dirs if x.is_dir()]
+    data = [utils.load_batches(d) for d in deduped_dirs]
+    data = [x for ds in data for x in ds]
+
+    for x in data:
+        program = tokenizer.detokenize(x['tokens'])
+        retokenized = tokenizer.tokenize(program)
+        assert x['tokens'] == retokenized, (
+            f"tokenize(detokenize(x)) does not equal x."
+        )
+
+
+def test_compilation(make_test_data):
+    pass
--- tests/test_tokenize.py
+++ tests/test_tokenize.py
@@ -4,12 +4,14 @@ import pytest
 
 from tracr.rasp import rasp
 
-from rasp_generator import rasp_utils
-from rasp_tokenizer import tokenizer
-from rasp_tokenizer import lib
+from decompile_tracr.sampling import rasp_utils
+from decompile_tracr.tokenizing import tokenizer
+from decompile_tracr.tokenizing import vocab
+from decompile_tracr.dataset import lib
 
 
 SAMPLE_PROGRAMS = lib.examples
+SAMPLE_PROGRAMS_TOKENIZED = [tokenizer.tokenize(x) for x in SAMPLE_PROGRAMS]
 
 
 @pytest.mark.parametrize("program", SAMPLE_PROGRAMS)
@@ -24,3 +26,33 @@ def test_round_trip(program: rasp.SOp):
     retokenized = tokenizer.tokenize(reconstructed)
     assert tokens == retokenized, (
         f"tokenize(reconstructed) does not equal tokenize(original).")
+
+
+@pytest.mark.parametrize("tokens", SAMPLE_PROGRAMS_TOKENIZED)
+def test_sop_names(tokens: list):
+    """
+    Test that ops are always preceded by an
+    encoding and a variable called SOp_{n}
+    """
+    decoded = [tokenizer.decode(layer) for layer in tokens]
+    for idx, tok in enumerate(decoded):
+        if tok in ["Map", "SequenceMap", "LinearSequenceMap", 
+                   "Aggregate", "SelectorWidth"]:
+            assert decoded[idx-1] in ["numerical", "categorical"], (
+                f"SOp {tok} preceded by {decoded[idx-1]} instead of "
+                "numerical or categorical."
+            )
+
+            assert decoded[idx-2].startswith("SOp_"), (
+                f"SOp {tok} named {decoded[idx-1]} instead of "
+                "SOp_n."
+            )
+
+
+@pytest.mark.parametrize("tokens", SAMPLE_PROGRAMS_TOKENIZED)
+def test_bos_and_eos(tokens: list):
+    """Test that each sequence begins with BOS and ends with EOS"""
+    for layer in tokens:
+        decoded = tokenizer.decode(layer)
+        assert decoded[0] == vocab.BOS, f"Expected BOS, got {decoded[0]}"
+        assert decoded[-1] == vocab.EOS, f"Expected EOS, got {decoded[-1]}"
