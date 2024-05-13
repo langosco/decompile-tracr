3923a23dc28976271aacf586f9b37a2c9997b494
Lauro Langosco di Langosco <langosco.lauro@gmail.com>
2024-05-13 15:37:15 +0100

    add dummy test script for autonecoder

---
+++ tests/test_autoencoder.py
@@ -0,0 +1,23 @@
+import jax
+import numpy as np
+
+from decompile_tracr.tokenizing import tokenizer
+from decompile_tracr.sampling import sampling
+from decompile_tracr.dataset import compile
+from decompile_tracr.training import autoencoder
+
+
+rng = np.random.default_rng(0)
+key = jax.random.key(0)
+
+
+def test_autoencoder():
+    # TODO
+    if False:
+        program_toks = tokenizer.tokenize(sampling.sample(rng, program_length=5))
+        assembled_model = compile.compile_tokens_to_model(program_toks)
+
+        # Train autoencoder to compress residual stream
+        key, subkey = jax.random.split(key)
+        ae_state, log, ae_model = autoencoder.train_autoencoder(
+            subkey, assembled_model, nsteps=10)
