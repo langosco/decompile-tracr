8db7689e390d0b74d5b8d4d3f7f99cd090bfe5bb
Lauro Langosco di Langosco <langosco.lauro@gmail.com>
2024-06-29 13:43:36 +0100

    update count script to use programs.h5 if datset.h5 does not exist

--- scripts/count.py
+++ scripts/count.py
@@ -9,16 +9,18 @@ parser.add_argument('--config', type=str, default=None,
                     help="Name of config file.")
 args = parser.parse_args()
 config = load_config(args.config)
+dataset = config.paths.dataset
+programs = config.paths.programs
 
-if not config.paths.dataset.exists():
-    raise FileNotFoundError(f"Dataset {config.paths.dataset} not found")
-
-with h5py.File(config.paths.dataset, 'r') as f:
-    for k, v in f.items():
-        print(f"Number of datapoints in {k}: {len(v['tokens']):,}")
-#    print()
-#    print("Dataset shapes:")
-#    for k, v in f.items():
-#        for kk, vv in v.items():
-#            print(f"    {k}/{kk}: {vv.shape}")
-
+if dataset.exists() and False:
+    with h5py.File(dataset, 'r') as f:
+        for k, v in f.items():
+            print(f"Number of datapoints in {k}: {len(v['tokens']):,}")
+else:
+    if not programs.exists():
+        raise FileNotFoundError(f"Dataset {dataset} not found")
+    else:
+        print(f"Dataset {dataset} not found. Counting programs "
+              f"in {programs} instead.")
+    with h5py.File(programs, 'r') as f:
+        print(f"Number of datapoints: {len(f['tokens']):,}")
